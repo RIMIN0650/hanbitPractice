cd73596f9400c26705b8033d6012efd547e64441
KwonLaptop <chris03232000@gmail.com>
2025-07-16 12:13:56 +0900

    [권민석]  - 스프링 초기 설정

---
+++ rimin/src/main/java/kr/co/rimin/RiminApplication.java
@@ -0,0 +1,13 @@
+package kr.co.rimin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiminApplication.class, args);
+	}
+
+}
---
+++ rimin/src/test/java/kr/co/rimin/RiminApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.rimin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
