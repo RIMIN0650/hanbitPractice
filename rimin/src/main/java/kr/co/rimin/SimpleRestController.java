c30684843d2c30a1c3c08e65857ce962a19ee5e5
KwonLaptop <chris03232000@gmail.com>
2025-07-16 14:01:38 +0900

    [권민석]  - 컨트롤러에 요청을 처리할 수 있는  @RequestMapping 추가

--- rimin/src/main/java/kr/co/rimin/SimpleRestController.java
+++ rimin/src/main/java/kr/co/rimin/SimpleRestController.java
@@ -20,11 +20,17 @@ public class SimpleRestController {
     // Ambiguous mapping이라는 에러 메세지가 나오면서 애플리케이션이 종료된다.
     // 동일한 경로로 2개의 컨트롤러를 등록하면 어떤 경로로 연결할지 모호하기 때문.
 
-    @RequestMapping("/")
-    public String hello(){
-        return "Hello <strong>Backend</strong>";
-    }
+//    @RequestMapping("/")
+//    public String hello(){
+//        return "Hello <strong>Backend</strong>";
+//    }
     // HTML로 작성한 코드도 출력 화면에 반영되는 것을 확인 가능.
+
+    @RequestMapping("/article")
+    public String createArticle(){
+        return "SOME MESSAGE";
+    }
+
 
 
 }
